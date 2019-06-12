cf078dda000d00d18334af5d6a198e007a32a6d6
liuheyong <17682347237@163.com>
2019-06-12 13:00:36 +0800

    commit

---
+++ src/main/java/com/cloud2/commons/constants/Constants.java
@@ -0,0 +1,33 @@
+package com.cloud2.commons.constants;
+
+/**
+ * @author: LiuHeYong
+ * @create: 2019-06-05
+ * @description: Constants
+ **/
+public class Constants {
+
+    /**
+     * 成功
+     */
+    public static final String RESULT_SUCCESS = "1";
+    /**
+     * 失败
+     */
+    public static final String RESULT_FAIL = "0";
+    /**
+     * boot标识
+     */
+    public static final String CLOUD2 = "cloud2";
+
+    /**
+     * 统一异常跳转页面
+     */
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    /**
+     * 100
+     */
+    public static final Integer NUMBER_100 = 100;
+
+}
